90a8b5a253ea58f41cc07c3bdcc9efd4c63d3f55
Kunal Asude <kunalasude@gmail.com>
2025-07-21 14:01:40 +0530

    Update core application files

--- farmsetu_weather_project/wsgi.py
+++ farmsetu_weather_project/wsgi.py
@@ -11,6 +11,15 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
+# Add the project directory to the Python path
+import sys
+from pathlib import Path
+
+# Calculate the project directory
+project_dir = Path(__file__).resolve().parent.parent
+sys.path.insert(0, str(project_dir))
+
+# Set the default Django settings module
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'farmsetu_weather_project.settings')
 
 application = get_wsgi_application()
